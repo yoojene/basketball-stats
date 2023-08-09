972b87d42ec8044a69308c4424acec7eb958c6e4
Eugene Cross <eugene.cross@gmail.com>
2023-08-09 17:24:44 +0100

    feat: api returns team images > add images to server

--- src/routes/api/teams.ts
+++ src/routes/api/teams.ts
@@ -1,28 +1,22 @@
 import express, { Request, Response } from "express";
 import { ENDPOINT } from "../../endpoint";
-
+import Finder from "fs-finder";
 const router = express.Router();
-
 router.get("/", async (req: Request, res: Response) => {
-
   try {
+    const perPage = req.query;
 
-    const perPage = req.query
-
-    let url = `${ENDPOINT}/teams`
+    let url = `${ENDPOINT}/teams`;
     if (Object.keys(perPage).length !== 0) {
       url = `${ENDPOINT}/teams?${Object.keys(req.query)[0]}=${
         req.query.per_page
       }`;
     }
-    console.log(url)
-    const data = await fetch(
-      url
-    );    
+    const data = await fetch(url);
     const body = await data.json();
 
-
-    if (!data) { // Not working properly yet
+    if (!data) {
+      // Not working properly yet
       res.status(400).json({
         message: "error",
         code: 400,
@@ -45,37 +39,53 @@ router.get("/", async (req: Request, res: Response) => {
   return;
 });
 
-
 router.get("/:id", async (req: Request, res: Response) => {
-  console.log(req.params);
   const teamId = req.params;
 
   if (Object.keys(teamId).length === 0) {
     res.status(400).json({
       message: "error",
       code: 400,
-      error: "Specify the team id in the url" ,
+      error: "Specify the team id in the url",
     });
     return;
   }
 
   try {
-    const data = await fetch(`${ENDPOINT}/teams/${teamId.id}`);    
-    const body = await data.json();
+    const data = await fetch(`${ENDPOINT}/teams/${teamId.id}`);
+    let body = await data.json();
 
+    const team = body.name.toLowerCase();
 
-    if (!data) { // Not working properly yet
+    // Search for team image / logo and add to payload
+    Finder.from("./images/teams").findFiles(team, (files: []) => {
+      if (files.length == 0) {
+        sendSuccess(body);
+      } else {
+
+        const [file] = files;
+
+        body = { ...body, ...{ logo: file } };
+        sendSuccess(body);
+      }
+    });
+
+    if (!data) {
+      // Not working properly yet
       res.status(400).json({
         message: "error",
         code: 400,
         error: "Sorry that is an invalid team ID",
       });
     }
-    res.json({
-      message: "Success",
-      code: 200,
-      data: body,
-    });
+
+    const sendSuccess = (body: any) => {
+      res.json({
+        message: "Success",
+        code: 200,
+        data: body,
+      });
+    };
   } catch (err) {
     console.error(err);
     res.status(500).json({
@@ -84,6 +94,6 @@ router.get("/:id", async (req: Request, res: Response) => {
       data: `There was an error ${err}`,
     });
   }
-});  
+});
 
 module.exports = router;
